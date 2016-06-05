39f10267da1f5cd8d38b770cee179a6bca5c4b97
Benjamin Brown <foolsh.person@gmail.com>
2016-06-05 00:19:20 -0400

    Hello File Information Utility

---
+++ file-info.c
@@ -0,0 +1,23 @@
+/*
+File Information Utility
+
+Benjamin Brown
+*/
+
+#include <stdio.h>
+
+int error_code;
+
+int main()
+{
+
+    error_code = 0;
+
+    printf("\n\tFile Information Utility\n");
+    printf("\tBy Ben Brown\n\n");
+
+    return error_code;
+
+}
+
+
