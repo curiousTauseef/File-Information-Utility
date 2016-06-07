c557e3471af4d5fc5409d5bc788831139a883eba
Benjamin Brown <foolsh.person@gmail.com>
2016-06-07 01:01:22 -0400

    Version 0.0 - Yea, it does things!

--- file-info.c
+++ file-info.c
@@ -7,8 +7,8 @@ Benjamin Brown
 #include <stdio.h>
 #include "file-info.h"
 
-double counted_chars = 0;
-double counted_lines = 0;
+double counted_realchars, counted_chars, counted_lines = 0;
+int ndigit[10];
 
 int main()
 {
@@ -17,20 +17,35 @@ int main()
 
     count_stats();
 
-    printf("\tTotal Counted Characters:\t%.0f\n\n", counted_chars);
-    printf("\t     Total Counted Lines:\t%.0f\n\n", counted_lines);
+
+
+
+    printf("\tTotal Counted Characters:\t%.0f\n", counted_realchars);
+    printf("\tTotal Visible Characters:\t%.0f\n", counted_chars);
+    printf("\t       Hidden Characters:\t%.0f\n", (counted_realchars - counted_chars));
+    printf("\t        Total Unix Lines:\t%.0f\n\n", counted_lines);
 
     return 0;
 }
 
 double count_stats()
 {
-    int c =0;
+    int c = 0;
 
     while ((c = getchar()) != EOF)
+    {
+        printf("%d", c);
+        ++counted_realchars;
         if (c == '\n')
             ++counted_lines;
         else
             ++counted_chars;
+    }
+    printf("\n\n");
     return 0;
 }
+
+
+
+
+
